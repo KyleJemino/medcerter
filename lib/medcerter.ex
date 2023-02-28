d1feff65ab04fc59995eb17941b253bbdd9a18de
KyleJemino <kcjemino@up.edu.ph>
2023-02-28 21:12:38 +0800

    adds basic_queries function

--- lib/medcerter.ex
+++ lib/medcerter.ex
@@ -6,4 +6,20 @@ defmodule Medcerter do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def basic_queries do
+    quote do
+      defp query_by(query, %{"id" => id} = params) do
+        query
+        |> where([q], q.id == ^id)
+        |> query_by(Map.delete(params, "id"))
+      end
+
+      defp query_by(query, %{"preload" => preload} = params) do
+        query
+        |> preload([q], ^preload)
+        |> query_by(Map.delete(params, "preload"))
+      end
+    end
+  end
 end
