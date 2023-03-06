1634fc4e2449b73cbaa76466996af718dc2a8873
KyleJemino <kcjemino@up.edu.ph>
2023-03-07 01:50:35 +0800

    fix references in migrations

--- priv/repo/migrations/20230303112227_add_visits.exs
+++ priv/repo/migrations/20230303112227_add_visits.exs
@@ -5,15 +5,15 @@ defmodule Medcerter.Repo.Migrations.AddVisits do
     create table(:visits, primary_key: false) do
       add :id, :binary_id, primary_key: true
       add :date, :utc_datetime
-      add :patient_id, references(:doctors, type: :binary_id, on_delete: :nothing)
-      add :doctor_id, references(:patients, type: :binary_id, on_delete: :nothing)
+      add :patient_id, references(:patients, type: :binary_id, on_delete: :nothing)
+      add :doctor_id, references(:doctors, type: :binary_id, on_delete: :nothing)
       add :history, :text
       add :archived_at, :utc_datetime
 
       timestamps()
     end
 
-    create unique_index(:visits, [:patient_id])
-    create unique_index(:visits, [:doctor_id])
+    create index(:visits, [:patient_id])
+    create index(:visits, [:doctor_id])
   end
 end
