e5b9dfa7462f0499dd07092a0588b9333bd0fbac
KyleJemino <kcjemino@up.edu.ph>
2023-03-07 21:48:08 +0800

    test create_visit/1

--- test/medcerter/visits_test.exs
+++ test/medcerter/visits_test.exs
@@ -5,6 +5,7 @@ defmodule Medcerter.VisitsTest do
     alias Medcerter.Visits
     alias Medcerter.Visits.Visit
     import Medcerter.VisitsFixtures 
+    import Medcerter.PatientsFixtures
 
     test "list_visits/0 returns all visits" do
       visit = visit_fixture()  
@@ -22,6 +23,36 @@ defmodule Medcerter.VisitsTest do
       visit = Visits.get_visit(id)
 
       assert visit.id === id
+    end
+
+    test "get_visit/1 returns nil if visit doesn't exist" do
+      assert is_nil(Visits.get_visit(Ecto.UUID.generate()))
+    end
+
+    test "create_visit/1 creates visit with valid attributes" do
+      %{id: patient_id, doctor_id: doctor_id} = patient_fixture()
+
+      valid_attrs = valid_visit_attrs(%{
+        patient_id: patient_id,
+        doctor_id: doctor_id,
+        history: "cancer"
+      })
+
+      assert {:ok, %Visit{} = visit} = Visits.create_visit(valid_attrs)
+      assert visit.patient_id === patient_id
+      assert visit.doctor_id === doctor_id
+      assert visit.history === "cancer"
+    end
+
+    test "create_visit/1 returns error changeset with invalid data" do
+      assert {:error, %Ecto.Changeset{} = changeset} =
+        Visits.create_visit(%{history: "hi"})
+
+      assert %{
+        date: ["can't be blank"],
+        doctor_id: ["can't be blank"],
+        patient_id: ["can't be blank"]
+      } = errors_on(changeset)
     end
   end
 end
--- test/support/fixtures/visits_fixtures.ex
+++ test/support/fixtures/visits_fixtures.ex
@@ -2,17 +2,22 @@ defmodule Medcerter.VisitsFixtures do
   alias Medcerter.PatientsFixtures, as: PF
   alias Medcerter.Visits
 
-  def visit_fixture(attrs \\ %{}) do
+  def valid_visit_attrs(attrs \\ %{}) do
     %{id: patient_id, doctor_id: doctor_id} = PF.patient_fixture()
 
-    valid_attrs = Enum.into(attrs, %{
+    Enum.into(attrs, %{
       date: DateTime.utc_now(),
       history: "lorem ipsum",
       patient_id: patient_id,
       doctor_id: doctor_id
     })
+  end
 
-    {:ok, visit} = Visits.create_visit(valid_attrs)
+  def visit_fixture(attrs \\ %{}) do
+    {:ok, visit} = 
+      attrs
+      |> valid_visit_attrs()
+      |> Visits.create_visit()
     visit
   end
 end
