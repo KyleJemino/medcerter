8f59bee6350c6bbb3fc167adadf805ed70faae76
KyleJemino <kcjemino@up.edu.ph>
2023-03-26 21:33:56 +0800

    adds clinic queries

---
+++ lib/medcerter/clinics/queries/clinic_query.ex
@@ -0,0 +1,10 @@
+defmodule Medcerter.Clinics.Queries.ClinicQuery do
+  import Ecto.Query
+  alias Medcerter.Clinics.Clinic
+
+  def query_clinic(params) do
+    query_by(Clinic, params)
+  end
+
+  use Medcerter, :basic_queries
+end
