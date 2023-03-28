13a104488d1b869ccef3aa88a31573cb1cb405c6
KyleJemino <kcjemino@up.edu.ph>
2023-03-28 14:59:19 +0800

    adds clinic_form_component

--- lib/medcerter_web/live/live_components/clinic_form_component.ex
+++ lib/medcerter_web/live/live_components/clinic_form_component.ex
@@ -13,4 +13,24 @@ defmodule MedcerterWeb.LiveComponents.ClinicFormComponent do
       |> assign(:changeset, changeset)
     }
   end
+
+  def handle_event("validate", %{"clinic" => clinic_params}, socket) do
+    changeset =
+      socket.assigns.clinic
+      |> Clinics.create_change_clinic(clinic_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
+
+  def handle_event("save", %{"clinic" => clinic_params}, socket) do
+    case Clinics.create_clinic(clinic_params) do
+      {:ok, _patient} ->
+        {:noreply,
+          socket
+          |> put_flash(:info, "Clinic created successfully")
+          |> push_redirect(to: socket.assigns.return_to)
+        }
+    end
+  end
 end
