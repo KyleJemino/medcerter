9f81b232b46b0394fdc1d76808280f1854ffbf20
KyleJemino <kcjemino@up.edu.ph>
2023-04-02 23:30:35 +0800

    adds show

--- lib/medcerter_web/live/clinic_live/show.ex
+++ lib/medcerter_web/live/clinic_live/show.ex
@@ -4,15 +4,21 @@ defmodule MedcerterWeb.ClinicLive.Show do
   alias Medcerter.Doctors
   alias Medcerter.Repo
   alias Medcerter.Clinics.Clinic
+  alias Medcerter.Clinics
 
   @impl true
-  def mount(params, _session, socket) do
-    IO.inspect params
-    {:ok, socket}
+  def mount(%{"clinic_id" => clinic_id}, _session, socket) do
+    {:ok, assign_clinic(socket, clinic_id)}
   end
 
   @impl true
   def handle_params(params, url, socket) do
     {:noreply, socket}
+  end
+
+  def assign_clinic(socket, clinic_id) do
+    assign_new(socket, :clinic, fn ->
+      Clinics.get_clinic(clinic_id)
+    end)
   end
 end
