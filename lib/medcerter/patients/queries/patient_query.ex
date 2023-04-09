eedade06d3c2f51442e87c047ef771d68849a4fd
KyleJemino <kcjemino@up.edu.ph>
2023-04-09 21:11:34 +0800

    reworks patients backend

--- lib/medcerter/patients/queries/patient_query.ex
+++ lib/medcerter/patients/queries/patient_query.ex
@@ -6,10 +6,10 @@ defmodule Medcerter.Patients.Queries.PatientQuery do
     query_by(Patient, params)
   end
 
-  defp query_by(query, %{"doctor_id" => doctor_id} = params) do
+  defp query_by(query, %{"clinic_id" => clinic_id} = params) do
     query
-    |> where([q], q.doctor_id == ^doctor_id)
-    |> query_by(Map.delete(params, "doctor_id"))
+    |> where([q], q.clinic_id == ^clinic_id)
+    |> query_by(Map.delete(params, "clinic_id"))
   end
 
   use Medcerter, :basic_queries
