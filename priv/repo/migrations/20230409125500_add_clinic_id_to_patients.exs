26df9602cddea0cdecaa343d8b339e219794b450
KyleJemino <kcjemino@up.edu.ph>
2023-04-09 22:11:52 +0800

    fix clinic_id migration

--- priv/repo/migrations/20230409125500_add_clinic_id_to_patients.exs
+++ priv/repo/migrations/20230409125500_add_clinic_id_to_patients.exs
@@ -3,7 +3,7 @@ defmodule Medcerter.Repo.Migrations.AddClinicIdToPatients do
 
   def change do
     alter table(:patients) do
-      add :clinic_id, references(:patients)
+      add :clinic_id, references(:clinics, type: :binary_id, on_delete: :nothing)
     end
 
     create index(:patients, [:clinic_id])
