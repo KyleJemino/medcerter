f40b86d18fb73b4c83aa03cdf7e3151f95083a43
KyleJemino <kcjemino@up.edu.ph>
2023-04-23 15:59:20 +0800

    adds query for doctor_clinic

--- lib/medcerter/clinics/queries/doctor_clinic_query.ex
+++ lib/medcerter/clinics/queries/doctor_clinic_query.ex
@@ -6,5 +6,17 @@ defmodule Medcerter.Clinics.Queries.DoctorClinicQuery do
     query_by(DoctorClinic, params)
   end
 
+  defp query_by(query, %{"doctor_id" => doctor_id} = params) do
+    query
+    |> where([q], q.doctor_id == ^doctor_id)
+    |> query_by(Map.delete(params, "doctor_id"))
+  end
+
+  defp query_by(query, %{"clinic_id" => clinic_id} = params) do
+    query
+    |> where([q], q.clinic_id == ^clinic_id)
+    |> query_by(Map.delete(params, "clinic_id"))
+  end
+
   use Medcerter, :basic_queries
 end
