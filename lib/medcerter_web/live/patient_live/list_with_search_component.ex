22947b89058e2a31c80d3d6b129bc3f40412bda6
KyleJemino <kcjemino@up.edu.ph>
2023-05-28 15:35:34 +0800

    adds patient live list with search component

---
+++ lib/medcerter_web/live/patient_live/list_with_search_component.ex
@@ -0,0 +1,19 @@
+defmodule MedcerterWeb.PatientLive.ListWithSearchComponent do
+  use MedcerterWeb, :live_component
+
+  alias Medcerter.Patients
+
+  @impl true
+  def update(%{base_params: base_params} = assigns, socket) do
+    {:ok,
+      socket
+      |> assign(assigns)
+      |> assign(:base_params, base_params)
+      |> assign_patients()
+    }
+  end
+
+  defp assign_patients(%{assigns: %{base_params: base_params}} = socket) do
+    assign(socket, :patients, Patients.list_patients(base_params))
+  end
+end
