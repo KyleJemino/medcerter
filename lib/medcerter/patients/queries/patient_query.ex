9fef3347568000cecf9874b648059559af286a01
KyleJemino <kcjemino@up.edu.ph>
2023-06-04 23:14:50 +0800

    adds search (WIP) has bug

--- lib/medcerter/patients/queries/patient_query.ex
+++ lib/medcerter/patients/queries/patient_query.ex
@@ -18,5 +18,44 @@ defmodule Medcerter.Patients.Queries.PatientQuery do
     |> query_by(Map.delete(params, "doctor_id"))
   end
 
+  defp query_by(
+    query, 
+    %{
+      "last_name" => last_name,
+    } = params
+  ) do
+    last_name_query = "%#{last_name}%"
+
+    query
+    |> where([q], ilike(q.last_name, ^last_name_query))
+    |> query_by(Map.delete(params, "last_name"))
+  end
+
+  defp query_by(
+    query, 
+    %{
+      "first_name" => first_name,
+    } = params
+  ) do
+    first_name_query = "%#{first_name}%"
+
+    query
+    |> where([q], ilike(q.first_name, ^first_name_query))
+    |> query_by(Map.delete(params, "first_name"))
+  end
+
+  defp query_by(
+    query, 
+    %{
+      "middle_name" => middle_name,
+    } = params
+  ) do
+    middle_name_query = "%#{middle_name}%"
+
+    query
+    |> where([q], ilike(q.middle_name, ^middle_name_query))
+    |> query_by(Map.delete(params, "middle_name"))
+  end
+
   use Medcerter, :basic_queries
 end
--- lib/medcerter_web/live/patient_live/list_with_search_component.ex
+++ lib/medcerter_web/live/patient_live/list_with_search_component.ex
@@ -5,21 +5,44 @@ defmodule MedcerterWeb.PatientLive.ListWithSearchComponent do
 
   @impl true
   def update(%{base_params: base_params} = assigns, socket) do
-    IO.inspect base_params
     {:ok,
       socket
       |> assign(assigns)
       |> assign(:base_params, base_params)
+      |> assign(:search_params, %{}) 
       |> assign_patients()
     }
   end
 
-  def handle_event("search", params, socket) do
-    IO.inspect params
-    {:noreply, socket}
+  def handle_event("search", %{ "search_params" => search_params }, socket) do
+    {:noreply, 
+      socket
+      |> assign_search_params(search_params)
+      |> assign_patients()
+    }
   end
 
-  defp assign_patients(%{assigns: %{base_params: base_params}} = socket) do
-    assign(socket, :patients, Patients.list_patients(base_params))
+  defp assign_patients(%{
+    assigns: %{
+      base_params: base_params,
+      search_params: search_params
+    }
+  } = socket) do
+    list_params = Map.merge(base_params, search_params)
+
+    assign(socket, :patients, Patients.list_patients(list_params))
+  end
+
+  defp assign_search_params(socket, search_params) do
+    search_params_with_values = 
+      search_params 
+      |> Enum.filter(
+        fn {_key, value} -> 
+          value !== ""
+        end
+      )
+      |> Map.new()
+
+    assign(socket, :search_params, search_params)
   end
 end
