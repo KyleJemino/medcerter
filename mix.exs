712897fd6fdebc832917ac79e195f5a4511ca3de
KyleJemino <kcjemino@up.edu.ph>
2023-06-08 23:19:32 +0800

    adds timex

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule Medcerter.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:dart_sass, "~> 0.5", runtime: Mix.env() == :dev}
+      {:dart_sass, "~> 0.5", runtime: Mix.env() == :dev},
+      {:timex, "~> 3.0"}
     ]
   end
 
