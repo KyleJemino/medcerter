16e8e98ba6f8e725e1de78c4f4a87dce65d33414
KyleJemino <kcjemino@up.edu.ph>
2024-01-07 21:47:49 +0800

    adds doctor, patient relation to prescription

--- priv/repo/migrations/20240107130616_add_prescriptions.exs
+++ priv/repo/migrations/20240107130616_add_prescriptions.exs
@@ -6,10 +6,14 @@ defmodule Medcerter.Repo.Migrations.AddPrescriptions do
       add :id, :binary_id, primary_key: true
       add :medicines, {:array, :map}
       add :visit_id, references(:visits, type: :binary_id, on_delete: :nothing), null: false
+      add :doctor_id, references(:doctors, type: :binary_id, on_delete: :nothing), null: false
+      add :patient_id, references(:patients, type: :binary_id, on_delete: :nothing), null: false
 
       timestamps()
     end
 
     create index(:prescriptions, [:visit_id])
+    create index(:prescriptions, [:doctor_id])
+    create index(:prescriptions, [:patient_id])
   end
 end
