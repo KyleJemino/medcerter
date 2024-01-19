abcd5098e69471cc15d1be6f2c72d5e24187bf6b
KyleJemino <kcjemino@up.edu.ph>
2024-01-19 19:26:17 +0800

    adds patient info to medcert

--- lib/medcerter/helpers/patient_helpers.ex
+++ lib/medcerter/helpers/patient_helpers.ex
@@ -4,4 +4,9 @@ defmodule Medcerter.Helpers.PatientHelpers do
   def get_full_name(%Patient{} = patient) do 
     "#{patient.last_name}, #{patient.first_name}#{if not is_nil(patient.middle_name), do: ", #{patient.middle_name}"}"
   end
+
+  def get_age(%Patient{ birth_date: birth_date }) do
+    today = Date.utc_today()
+    today.year - birth_date.year
+  end
 end
--- lib/medcerter_web/live/components/prescription_components.ex
+++ lib/medcerter_web/live/components/prescription_components.ex
@@ -6,38 +6,49 @@ defmodule MedcerterWeb.Components.PrescriptionComponents do
   def prescription_card(assigns) do
     ~H"""
     <div class="prescription-card">
-      <div class="patient-information">
-        <div class="patient-info-item">
-          <span class="key">
-            Name:
-          </span>
-          <span class="value">
-            <%= PatientHelpers.get_full_name(@patient) %>
-          </span>
+      <div class="patient-information-container">
+        <div class="row">
+          <div class="patient-info-item -long">
+            <span class="key">
+              Name:
+            </span>
+            <span class="value">
+              <%= PatientHelpers.get_full_name(@patient) %>
+            </span>
+          </div>
+          <div class="patient-info-item">
+            <span class="key">
+              Age:
+            </span>
+            <span class="value">
+              <%= PatientHelpers.get_age(@patient) %>
+            </span>
+          </div>
+          <div class="patient-info-item">
+            <span class="key">
+              Sex:
+            </span>
+            <span class="value">
+              <%= String.upcase("#{@patient.sex}") %>
+            </span>
+          </div>
         </div>
-        <div class="info">
-          <span class="key">
-          </span>
-          <span class="value">
-          </span>
-        </div>
-        <div class="info">
-          <span class="key">
-          </span>
-          <span class="value">
-          </span>
-        </div>
-        <div class="info">
-          <span class="key">
-          </span>
-          <span class="value">
-          </span>
-        </div>
-        <div class="info">
-          <span class="key">
-          </span>
-          <span class="value">
-          </span>
+        <div class="row">
+          <div class="patient-info-item -long">
+            <span class="key">
+              Address:
+            </span>
+            <span class="value">
+            </span>
+          </div>
+          <div class="patient-info-item">
+            <span class="key">
+              Date:
+            </span>
+            <span class="value">
+              <%= Timex.format!(Date.utc_today(), "{0M}/{0D}/{YYYY}") %>
+            </span>
+          </div>
         </div>
       </div>
     </div>
