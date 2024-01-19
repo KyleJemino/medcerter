b26c13759fab18b82dab484bdd8a62c44cb91378
KyleJemino <kcjemino@up.edu.ph>
2024-01-19 08:12:32 +0800

    adds name to prescription

--- lib/medcerter_web/live/components/prescription_components.ex
+++ lib/medcerter_web/live/components/prescription_components.ex
@@ -1,10 +1,45 @@
 defmodule MedcerterWeb.Components.PrescriptionComponents do
   use Phoenix.Component
   use Phoenix.HTML
+  alias Medcerter.Helpers.PatientHelpers
 
   def prescription_card(assigns) do
     ~H"""
     <div class="prescription-card">
+      <div class="patient-information">
+        <div class="patient-info-item">
+          <span class="key">
+            Name:
+          </span>
+          <span class="value">
+            <%= PatientHelpers.get_full_name(@patient) %>
+          </span>
+        </div>
+        <div class="info">
+          <span class="key">
+          </span>
+          <span class="value">
+          </span>
+        </div>
+        <div class="info">
+          <span class="key">
+          </span>
+          <span class="value">
+          </span>
+        </div>
+        <div class="info">
+          <span class="key">
+          </span>
+          <span class="value">
+          </span>
+        </div>
+        <div class="info">
+          <span class="key">
+          </span>
+          <span class="value">
+          </span>
+        </div>
+      </div>
     </div>
     """
   end
