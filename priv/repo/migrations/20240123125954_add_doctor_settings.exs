3f4a611277635743bd8a0379fa3f02f6239464cb
KyleJemino <kcjemino@up.edu.ph>
2024-01-23 22:12:28 +0800

    add primary_key to id in migration

--- priv/repo/migrations/20240123125954_add_doctor_settings.exs
+++ priv/repo/migrations/20240123125954_add_doctor_settings.exs
@@ -3,7 +3,7 @@ defmodule Medcerter.Repo.Migrations.AddDoctorSettings do
 
   def change do
     create table(:doctor_settings, primary_key: false) do
-      add :id, :binary_id, primary_key: false
+      add :id, :binary_id, primary_key: true
       add :prescription_header, :string, null: false
       add :addresses_and_contacts, {:array, :map}, null: false
       add :license_no, :string, null: false
