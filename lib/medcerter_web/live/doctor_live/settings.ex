58dc2f961771646035b6753fddbcefb106a82379
KyleJemino <kcjemino@up.edu.ph>
2024-01-24 22:36:34 +0800

    adds route for doctor setting

---
+++ lib/medcerter_web/live/doctor_live/settings.ex
@@ -0,0 +1,13 @@
+defmodule MedcerterWeb.DoctorLive.Settings do
+  use MedcerterWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(_params, _url, socket) do
+    {:noreply, socket}
+  end
+end
