a32a2792b79970709dd36e05ec1e8d722f5c2870
KyleJemino <kcjemino@up.edu.ph>
2024-01-28 19:25:12 +0800

    removes settings

--- lib/medcerter/settings.ex
+++
@@ -1,7 +0,0 @@
-defmodule Medcerter.Settings do
-  alias Medcerter.Settings.Resolvers.DoctorSettingResolver, as: DSR
-
-  defdelegate get_doctor_setting_by_params(params \\ %{}), to: DSR
-  defdelegate create_doctor_setting(attrs \\ %{}), to: DSR
-  defdelegate change_doctor_setting(doctor_setting, attrs \\ %{}), to: DSR
-end
